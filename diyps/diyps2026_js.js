b53b4401eb1d58a21eb95881b9db77b0811c7b1b
itzomn <alice.a.tran@sjsu.edu>
2026-04-19 21:03:26 -0700

    g

---
+++ diyps/diyps2026_js.js
@@ -0,0 +1,80 @@
+var bg;
+var img3, img2;
+var initials ='jm'; // your initials
+var choice = '1'; // starting choice, so it is not empty
+var screenbg = 25; // off white background
+var lastscreenshot=61; // last screenshot never taken
+
+function preload() {
+// preload() runs once, it may make you wait
+//  img = loadImage('cat.jpg');  // cat.jpg needs to be next to this .js file
+// you can link to an image on your github account
+  bg = loadImage('https://itzomn.github.io/diyps/lounge.jpg');
+  img3 = loadImage('https://itzomn.github.io/diyps/cat3.png');
+  img2 = loadImage('https://itzomn.github.io/diyps/cat2.png');
+}
+
+function setup() {
+createCanvas(600, 600);  // canvas size
+background(bg);   // use our background screen color
+
+}
+
+function draw() {
+  if (keyIsPressed) {
+    choice = key; // set choice to the key that was pressed
+    clear_print(); // check to see if it is clear screen or save image
+  }
+  if (mouseIsPressed){
+    newkeyChoice(choice);  // if the mouse is pressed call newkeyChoice
+  }
+}
+
+function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
+  // the key mapping if statements that you can change to do anything you want.
+  // just make sure each key option has the a stroke or fill and then what type of 
+  // graphic function
+
+ if (toolChoice == '1' ) {  // first tool
+   
+    image(img3, mouseX, mouseY, 50, 50);
+  
+  } else if (toolChoice == '2' || toolChoice == '2') { // g places the image we pre-loaded
+    image(img2, mouseX, mouseY, 50, 50);
+   
+   } else if (toolChoice == '3' || toolChoice == '3') { // o places the image we pre-loaded
+    image(img3, mouseX, mouseY, 50, 50);
+    
+  }
+ }
+ 
+function testbox(r, g, b) {
+// this is a test function that will show you how you can put your own functions into the sketch
+  x = mouseX;
+  y = mouseY;
+  fill(r, g, b);
+  rect(x-50, y-50, 100, 100);
+
+}
+
+function clear_print() {
+// this will do one of two things, x clears the screen by resetting the background
+// p calls the routine saveme, which saves a copy of the screen
+  if (key == 'x' || key == 'X') {
+    background(screenbg); // set the screen back to the background color
+  } else if (key == 'p' || key == 'P') {
+     saveme();  // call saveme which saves an image of the screen
+  }
+}
+
+function saveme(){
+    //this will save the name as the intials, date, time and a millis counting number.
+    // it will always be larger in value then the last one.
+  filename=initials+day() + hour() + minute() +second();
+  if (second()!=lastscreenshot) { // don't take a screenshot if you just took one
+    saveCanvas(filename, 'jpg');
+    key="";
+  }
+  lastscreenshot=second(); // set this to the current second so no more than one per second
+  
+}
